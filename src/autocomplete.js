2c01ab15bc4a8ba2fd0bbf5450020c757064aedd
Michael Chaney <mdchaney@michaelchaney.com>
2023-08-15 21:33:09 -0500

    Modified autocomplete to work with json list.

--- src/autocomplete.js
+++ src/autocomplete.js
@@ -16,6 +16,8 @@ export default class Autocomplete extends Controller {
   }
   static uniqOptionId = 0
 
+  item_count = 0
+
   connect() {
     this.close()
 
@@ -68,10 +70,12 @@ export default class Autocomplete extends Controller {
       previouslySelected.classList.remove(...this.selectedClassesOrDefault)
     }
 
-    target.setAttribute("aria-selected", "true")
-    target.classList.add(...this.selectedClassesOrDefault)
-    this.inputTarget.setAttribute("aria-activedescendant", target.id)
-    target.scrollIntoView({ behavior: "auto", block: "nearest" })
+    if (this.item_count > 0 && !target.classList.contains("disabled")) {
+      target.setAttribute("aria-selected", "true")
+      target.classList.add(...this.selectedClassesOrDefault)
+      this.inputTarget.setAttribute("aria-activedescendant", target.id)
+      target.scrollIntoView({ behavior: "auto", block: "nearest" })
+    }
   }
 
   onKeydown = (event) => {
@@ -197,8 +201,8 @@ export default class Autocomplete extends Controller {
     const url = this.buildURL(query)
     try {
       this.element.dispatchEvent(new CustomEvent("loadstart"))
-      const html = await this.doFetch(url)
-      this.replaceResults(html)
+      const json_response = await this.doFetch(url)
+      this.replaceResults(json_response, query)
       this.element.dispatchEvent(new CustomEvent("load"))
       this.element.dispatchEvent(new CustomEvent("loadend"))
     } catch(error) {
@@ -233,8 +237,8 @@ export default class Autocomplete extends Controller {
       throw new Error(`Server responded with status ${response.status}`)
     }
 
-    const html = await response.text()
-    return html
+    const json_response = await response.json()
+    return json_response
   }
 
   abortLastRequest() {
@@ -244,14 +248,145 @@ export default class Autocomplete extends Controller {
     }
   }
 
-  replaceResults(html) {
-    this.resultsTarget.innerHTML = html
+  // We will create a series of <li> elements, each of which will have
+  // a data-autocomplete-value attribute.  Additionally, I'll add a
+  // class of "list-group-item" (which should be a param or something)
+  // as well as "role="option".
+  //
+  // Note that "json_response" is a hash with keys:
+  // "items" is a list of all matching items
+  // "exact_items" is a list of items that exactly match the query
+  // "field" is the name of the field that was searched
+  // "raw_term" is the term that was searched for
+  // "term" is the term that was searched for, but with any special characters
+  //  escaped
+  //  "partial" is a boolean indicating whether the search was partial
+  replaceResults(json_response, query) {
+    const all_items = this.getUniqueItemList(json_response)
+    this.updateResults(all_items, query)
+
     this.identifyOptions()
     if (!!this.options) {
       this.open()
+      this.setHeight()
     } else {
       this.close()
     }
+  }
+
+  // updateResults will take a list of all items and update the DOM
+  // to reflect the new list of items.
+  updateResults(items, query) {
+    this.item_count = 0
+    let last_li = null
+    const existing_lis = this.options
+    const lower_query = query.toLowerCase()
+    let shown_items = [];
+    for (var item of items) {
+      const pos = item.toLowerCase().indexOf(lower_query);
+      if (pos >= 0) {
+        const prefix = item.slice(0, pos)
+        const match = item.slice(pos, pos + query.length)
+        const suffix = item.slice(pos + query.length)
+        const prefix_txt = document.createTextNode(prefix)
+        const match_txt = document.createTextNode(match)
+        const match_em = document.createElement("em")
+        match_em.appendChild(match_txt)
+        const suffix_txt = document.createTextNode(suffix)
+        const existing_li = existing_lis.find(li => li.dataset.autocompleteValue === item)
+        if (existing_li) {
+          last_li = existing_li
+          while (existing_li.firstChild) {
+            existing_li.removeChild(existing_li.firstChild)
+          }
+          existing_li.appendChild(prefix_txt)
+          existing_li.appendChild(match_em)
+          existing_li.appendChild(suffix_txt)
+        } else {
+          const li = document.createElement("li")
+          li.classList.add("list-group-item")
+          li.dataset.autocompleteValue = item
+          li.setAttribute("role", "option")
+          li.appendChild(prefix_txt)
+          li.appendChild(match_em)
+          li.appendChild(suffix_txt)
+          if (last_li) {
+            this.resultsTarget.insertBefore(li, last_li.nextSibling)
+          } else {
+            this.resultsTarget.insertBefore(li, this.resultsTarget.firstChild)
+          }
+          last_li = li
+        }
+        this.item_count += 1
+        shown_items.push(item)
+      }
+    }
+
+    // Remove any <li> elements that are no longer needed.
+    for (var li of existing_lis) {
+      if (!shown_items.includes(li.dataset.autocompleteValue)) {
+        this.resultsTarget.removeChild(li)
+      }
+    }
+
+    // If there are no items, then we should hide the results.
+    if (this.item_count === 0) {
+      this.showSorryMessage(query)
+    } else {
+      this.hideSorryMessage()
+    }
+
+    // If there is only one item, then we should select it.
+    if (this.item_count === 1) {
+      this.selectOption(last_li)
+    }
+  }
+
+  // If there are no results, then we should show a "sorry" message.
+  // The sorry message will be shown as an li with a class of
+  // "list-group-item" and a class of "disabled".
+  // Note that when the sorry message is shown the other list items
+  // have already been removed.
+  showSorryMessage(query) {
+    const li = document.createElement("li")
+    li.classList.add("list-group-item", "disabled", "sorry-message")
+    li.ariaDisabled = true
+    li.appendChild(document.createTextNode(`No results found for "${query}"`))
+    this.resultsTarget.appendChild(li)
+    this.setHeight()
+  }
+
+  hideSorryMessage() {
+    const li = this.resultsTarget.querySelector("li.disabled.sorry-message")
+    if (li) {
+      this.resultsTarget.removeChild(li)
+    }
+  }
+
+  // setHeight will set the height of the resultsTarget to be the
+  // height of the first item times the number of items.
+  // If there are 5 or fewer items, then the height will be cleared
+  // so that the resultsTarget will be as tall as it needs to be.
+  // If there are more than 5 items, we will set the height of the
+  // resultsTarget to be the height of the first item times five.
+  setHeight() {
+    const li = this.resultsTarget.querySelector("li")
+    if (li) {
+      const li_height = li.offsetHeight
+      if (this.item_count <= 5) {
+        this.resultsTarget.style.height = ""
+      } else {
+        this.resultsTarget.style.height = `${li_height * 5}px`
+      }
+    }
+  }
+
+  // The unique item list will have the exact matches first, followed by
+  // the partial matches.
+  getUniqueItemList(json_response) {
+    const exact_items = json_response.exact_items
+    const partial_items = json_response.items.filter(item => !exact_items.includes(item))
+    return exact_items.concat(partial_items)
   }
 
   open() {
